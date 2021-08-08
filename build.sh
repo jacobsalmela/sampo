d070a4fd1899d67d49383ded40e22d0afedc262f
Jacob Salmela <me@jacobsalmela.com>
2021-08-08 10:16:01 -0500

    build enhancements logic
    Signed-off-by: Jacob Salmela <me@jacobsalmela.com>

--- build.sh
+++ build.sh
@@ -1,35 +1,76 @@
 #!/usr/bin/env bash
 source ./vars.bash
 
+# Run cleanup function in interrupt
+trap cleanup SIGINT
+
 # Get the full directory name of the script no matter where it is being called from
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-test_title() {
-  local test
-  local test_length
-  local width
-  local fill
-  # First argument passed is just a strings
-  test="$1"
-  # Get character length of string
-  test_length="$(echo -n "$1" | wc -m | sed 's/^[[:space:]]*//g')"
-  # Get width of terminal
-  width="$(tput cols)"
-  # Subtract the length of the string from the width of the terminal
-  stuff=$( expr ${width} - ${test_length} )
-  # Variable to hold a character to fill the line
-  fill="$(printf '%*s\n' ${stuff} '' | tr ' ' \#)"
-  echo "$test$fill"
+# anything below this line with a #/ will show up in the usage line
+#/ Usage: build.sh [-h] -[d|k] [-c]
+#/
+#/   A shell script that builds the sampo software
+#/
+#/    -h      show this help
+#/    -d      build and run this in docker
+#/    -k      build and run this in kubernetes (Docker for Mac)
+#/    -c      cleanup any previous runs
+
+usage() {
+  grep '^#/' "$0" | cut -c4-
 }
 
-# cd into the build directory
-pushd docker/$APP >/dev/null || exit
-# Build the image
-docker build -t $APP:$VERSION .
-# exit directory
-popd >/dev/null || exit
+cleanup() {
+  echo "Running cleanup..."
+  for p in $(ps aux | grep port-forward | grep -v grep | awk '{print $2}'); do kill -9 $p; done 2>/dev/null
+  kubectl delete -f $APP/ 2>/dev/null
+  docker rm "$(docker stop "$(docker ps -a | awk -v i="^$APP.*" '{if($2~i){print$1}}')" 2>/dev/null)" 2>/dev/null
+}
 
-if [[ "$1" == "k" ]]; then
+docker_build() {
+  if ! eval command -v docker 1>/dev/null; then
+    echo "docker needs to be installed"
+    exit 1
+  fi
+  # cd into the build directory
+  pushd docker/$APP >/dev/null || exit 1
+    # Build the image
+    docker build -t $APP:$VERSION .
+  # exit directory
+  popd >/dev/null || exit 1
+}
+
+run_unit_tests() {
+  echo -e "\nRunning test suite"
+  # BATS doesn't always work, but it's a nice quick inidicator if things are decent while developing this.
+  bats "$DIR/test/$APP.bats"
+}
+
+# test_title() {
+#   local test
+#   local test_length
+#   local width
+#   local fill
+#   # First argument passed is just a strings
+#   test="$1"
+#   # Get character length of string
+#   test_length="$(echo -n "$1" | wc -m | sed 's/^[[:space:]]*//g')"
+#   # Get width of terminal
+#   width="$(tput cols)"
+#   # Subtract the length of the string from the width of the terminal
+#   stuff=$( expr ${width} - ${test_length} )
+#   # Variable to hold a character to fill the line
+#   fill="$(printf '%*s\n' ${stuff} '' | tr ' ' \#)"
+#   echo "$test$fill"
+# }
+
+build_k8s() {
+  if ! eval command -v kubectl 1>/dev/null; then
+    echo "kubectl needs to be installed"
+    exit 1
+  fi
+  docker_build
   # Kill any port-forwarding processes
   for p in $(ps aux | grep port-forward | grep -v grep | awk '{print $2}'); do kill -9 $p; done
   # Delete the old deployment
@@ -57,7 +98,12 @@ if [[ "$1" == "k" ]]; then
   kubectl port-forward --address 0.0.0.0 "$POD" $PORT:$PORT >/dev/null &
   # kubectl port-forward --address 0.0.0.0 deployment/"$DEPLOYMENT" :$PORT >/dev/null &
   # ps aux | grep port-forward | grep -v grep
-else
+  sleep 3
+  run_unit_tests
+}
+
+build_docker() {
+  docker_build
   # SCREEN_REGEX="[0-9]*\.$APP"
   # Cleanup from last run
   # Remove any screen sessions
@@ -69,18 +115,28 @@ else
     # Stop all by a specific version
     # docker rm "$(docker stop "$(docker ps -a -q --filter ancestor=$APP:$VERSION --format="{{.ID}}")")"
     # Stop all by image name only
-    docker rm "$(docker stop "$(docker ps -a | awk -v i="^$APP.*" '{if($2~i){print$1}}')")" 2>/dev/null
+    docker rm "$(docker stop "$(docker ps -a | awk -v i="^$APP.*" '{if($2~i){print$1}}')" 2>/dev/null)" 2>/dev/null
     # Run it in a docker container by default, mounting the examples directory, which contains all the scripts
   fi
   docker run -d \
     -v "$(pwd)"/examples:/$APP \
     -p $LOCAL_PORT:$PORT \
     $APP:$VERSION
-fi
+  sleep 3
+  run_unit_tests
+}
 
-echo "Running unit tests..."
-# BATS doesn't always work, but it's a nice quick inidicator if things are decent while developing this.
-sleep 3
-# Unit Tests
-# test_title "[TEST]: Verify 'echo' endpoint returns <value>"
-bats "$DIR/test/$APP.bats"
+while getopts ":hdkc" opt; do
+  case ${opt} in
+    h ) usage
+      ;;
+    d ) build_docker
+      ;;
+    k ) build_k8s
+      ;;
+    c ) cleanup
+      ;;
+    * ) usage
+      ;;
+  esac
+done

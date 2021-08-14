b34f0d40fb474b16aacf22238a081135a1c5176d
Jacob Salmela <me@jacobsalmela.com>
2021-08-14 07:55:48 -0500

    build improvements
    Signed-off-by: Jacob Salmela <me@jacobsalmela.com>

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+#shellcheck source=./vars.bash
 source ./vars.bash
 
 # Run cleanup function in interrupt
@@ -23,8 +24,9 @@ usage() {
 
 cleanup() {
   echo "Running cleanup..."
-  for p in $(ps aux | grep port-forward | grep -v grep | awk '{print $2}'); do kill -9 $p; done 2>/dev/null
-  kubectl delete -f $APP/ 2>/dev/null
+  #shellcheck disable=SC2009
+  for p in $(ps aux | grep port-forward | grep -v grep | awk '{print $2}'); do kill -9 "$p"; done 2>/dev/null
+  kubectl delete -f "$APP"/ 2>/dev/null
   docker rm "$(docker stop "$(docker ps -a | awk -v i="^$APP.*" '{if($2~i){print$1}}')" 2>/dev/null)" 2>/dev/null
 }
 
@@ -34,9 +36,9 @@ docker_build() {
     exit 1
   fi
   # cd into the build directory
-  pushd docker/$APP >/dev/null || exit 1
+  pushd docker/"$APP" >/dev/null || exit 1
     # Build the image
-    docker build -t $APP:$VERSION .
+    docker build -t "$APP":"$VERSION" .
   # exit directory
   popd >/dev/null || exit 1
 }
@@ -74,28 +76,29 @@ build_k8s() {
   # Kill any port-forwarding processes
   for p in $(ps aux | grep port-forward | grep -v grep | awk '{print $2}'); do kill -9 $p; done
   # Delete the old deployment
-  kubectl delete -f $APP/ >/dev/null
+  kubectl delete -f "$APP"/ >/dev/null
   # Create the deployment
-  kubectl create -f $APP/
+  kubectl create -f "$APP"/
   # Wait for the pod to come up
   echo "Waiting for pod to be ready..."
   # Until Running is found in the output of the kubectl command
   until [[ "$RUNNING_POD" == *Running* ]]
   do
     # Check the pods until they are Running
-    RUNNING_POD="$(kubectl get pod -l app=$APP --field-selector=status.phase==Running)"
+    RUNNING_POD="$(kubectl get pod -l app="$APP" --field-selector=status.phase==Running)"
     # RUNNING_POD="$(kubectl get pods -o custom-columns=NAMESPACE:metadata.namespace,POD:metadata.name,PodIP:status.podIP,READY-true:status.containerStatuses[*].ready | grep true)"
     echo "Terminiating.."
   done
   # Get the pod name in a variable
   # POD=$(kubectl get pod -l app=$APP --field-selector=status.phase==Running -o jsonpath="{.items[0].metadata.name}")
-  POD=$(kubectl get pods -l app=$APP -o=go-template --template='{{range .items}}{{$ready:=true}}{{range .status.containerStatuses}}{{if not .ready}}{{$ready = false}}{{end}}{{end}}{{if $ready}}{{.metadata.name}}{{"\n"}}{{end}}{{end}}')
+  #shellcheck disable=SC2016
+  POD=$(kubectl get pods -l app="$APP" -o=go-template --template='{{range .items}}{{$ready:=true}}{{range .status.containerStatuses}}{{if not .ready}}{{$ready = false}}{{end}}{{end}}{{if $ready}}{{.metadata.name}}{{"\n"}}{{end}}{{end}}')
   # DEPLOYMENT=$(kubectl get deployment -l app=$APP -o=go-template --template='{{range .items}}{{$ready:=true}}{{range .status.containerStatuses}}{{if not .ready}}{{$ready = false}}{{end}}{{end}}{{if $ready}}{{.metadata.name}}{{"\n"}}{{end}}{{end}}')
   # echo "Enabling port forwarding for $DEPLOYMENT..."
   echo "Enabling port forwarding for $POD..."
   # Set up local port forwarding in a screen for that pod
   # screen -d -S $APP -m kubectl port-forward $POD $PORT:$PORT
-  kubectl port-forward --address 0.0.0.0 "$POD" $PORT:$PORT >/dev/null &
+  kubectl port-forward --address 0.0.0.0 "$POD" "$PORT":"$PORT" >/dev/null &
   # kubectl port-forward --address 0.0.0.0 deployment/"$DEPLOYMENT" :$PORT >/dev/null &
   # ps aux | grep port-forward | grep -v grep
   sleep 3
@@ -109,22 +112,35 @@ build_docker() {
   # Remove any screen sessions
   # for session in $(screen -ls | grep -o \'$SCREEN_REGEX\'); do screen -S "${session}" -X quit; done >/dev/null
   # check for running containers of sampo
-  running_containers=$(docker ps -a | awk -v i="^$APP.*" '{if($2~i){print$1}}')
+  # running_containers=$(docker ps -a | awk -v i="^$APP.*" '{if($2~i){print$1}}')
+  running_containers=$(docker ps -a -q --filter ancestor="$APP":"$VERSION" --format="{{.ID}}")
   if [[ -n "$running_containers" ]]; then
     # Stop and remove all older running containers
     # Stop all by a specific version
     # docker rm "$(docker stop "$(docker ps -a -q --filter ancestor=$APP:$VERSION --format="{{.ID}}")")"
     # Stop all by image name only
-    docker rm "$(docker stop "$(docker ps -a | awk -v i="^$APP.*" '{if($2~i){print$1}}')" 2>/dev/null)" 2>/dev/null
+    for c in $running_containers
+    do
+      echo "Stopping and removing old container: $c"
+      docker rm "$(docker stop "$c" 2>/dev/null)" 2>/dev/null
+    done
     # Run it in a docker container by default, mounting the examples directory, which contains all the scripts
   fi
   docker run -d \
-    -v "$(pwd)"/examples:/$APP \
-    -p $LOCAL_PORT:$PORT \
-    $APP:$VERSION
+    -v "$(pwd)"/examples:/"$APP" \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -p "$LOCAL_PORT":"$PORT" \
+    "$APP":"$VERSION"
   sleep 3
   run_unit_tests
+  echo -e "To debug this container, run:\n"
+  echo "    docker exec -it $(docker ps -a | awk -v i="^sampo.*" '{if($2~i){print$1}}') bash"
 }
+
+if [[ $# -eq 0 ]]; then
+  usage
+  exit 1
+fi
 
 while getopts ":hdkc" opt; do
   case ${opt} in
